6daee85721dec9469ed7092ca4d3dd39da08f1ac
Muhammad Abubakar Siddique Ansari <162357493+ansarionline@users.noreply.github.com>
2025-01-04 20:38:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from nicegui import ui
 import os
+@ui.page('/')
 def home_page():
-    ui.label('Welcome to my NiceGUI app!').style('font-size: 24px;')
-    ui.button('Click Me', on_click=lambda: ui.notify('Button clicked!')).style('font-size: 18px;')
-ui.add_route('/', home_page)
+    ui.label('Welcome to my NiceGUI app!'
+    ).style('font-size: 24px;')
+    ui.button('Click Me', on_click=lambda: 
+    ui.notify('It is working',position='center',type='positive')
+    ).style('font-size: 18px;')
 if __name__ in {'__main__','__mp_main__'}:
     port = int(os.environ.get('PORT', 5000))
     ui.run(host='0.0.0.0', port=port)
